318ed434ce289eecf1634d74def019ccc6eb211e
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-02-05 21:00:59 +0530

    update

--- 7_Arrays/ArrayMethods/method.js
+++ 7_Arrays/ArrayMethods/method.js
@@ -2,3 +2,53 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let fruit = fruits.at(-1); // return last indexed element
 console.log(typeof fruit); // string
 console.log(fruit);
+
+
+
+console.log("----------------------------------------");
+console.log(fruits);
+console.log("adding kiwi");
+let len = fruits.push("Kiwi"); // it will return the length of the array after adding the element.
+console.log(fruits);
+console.log("-------------------------------------------");
+console.log("removing last indexed value");
+const deletedValueLast = fruits.pop(); // remove the last indexed value and return last indexed element.
+console.log(fruits);
+
+
+
+console.log("----------------------------------------")
+console.log(fruits);
+console.log('removing element from front');
+const deletedValueFront = fruits.shift(); // remove the first indexed value and return first indexed element.
+console.log(fruits);
+console.log("front deleted value is : "+ deletedValueFront);
+
+
+
+
+console.log("----------------------------------------")
+
+
+
+
+
+
+console.log("----------------------------------------")
+
+
+
+
+
+
+
+console.log("----------------------------------------")
+
+
+
+
+
+
+console.log("----------------------------------------")
+
+
